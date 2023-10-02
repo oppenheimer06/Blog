aba8f4f9a39adffe6159a1ecbc2f67b8fa7f8f06
oppenheimer <143889266+oppenheimer06@users.noreply.github.com>
2023-10-03 00:24:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -117,9 +117,9 @@ app.post("/compose",upload.single("image"), function(req,res){
   res.redirect("/");
 });
 
-
+module.exports=app;
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
 
-module.exports=app;
+
